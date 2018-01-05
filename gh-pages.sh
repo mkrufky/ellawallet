87c3e2e49ae19f89096242dd60aae3ee66adcfa2
Ellaismer <ellaismer@protonmail.ch>
2018-01-05 12:34:51 -0500

    Add gh-pages.sh deployment script

---
+++ gh-pages.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+git add -f ./dist
+TREE_OBJ_ID=`git write-tree --prefix=dist`
+git reset -- ./dist
+COMMIT_ID=`git commit-tree -p gh-pages -m "Release gh-pages" $TREE_OBJ_ID`
+git update-ref refs/heads/gh-pages $COMMIT_ID
