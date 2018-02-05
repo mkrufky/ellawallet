eb95a1e198c5939ca6839fe81911cc5227df6009
Ellaismer <ellaismer@protonmail.ch>
2018-02-05 08:39:27 -0500

    Auto push in deploy script

--- gh-pages.sh
+++ gh-pages.sh
@@ -7,3 +7,4 @@ TREE_OBJ_ID=`git write-tree --prefix=dist`
 git reset -- ./dist
 COMMIT_ID=`git commit-tree -p gh-pages -m "Release gh-pages" $TREE_OBJ_ID`
 git update-ref refs/heads/gh-pages $COMMIT_ID
+git push origin gh-pages
